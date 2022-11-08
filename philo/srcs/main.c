c1d1af4ab6fc7e69820e7ce65967e70b376576a8
uninstall-lea <lea.bisson@gmail.com>
2022-11-08 13:23:35 +0100

    add watcher

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -3,14 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lbisson <lbisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 16:59:58 by lea               #+#    #+#             */
-/*   Updated: 2022/11/05 17:15:52 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/08 13:18:05 by lbisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+void	watcher(void)
+{
+	int		i;
+	t_data	*data;
+	t_philo	*philo;
+
+	data = _data();
+	while (1)
+	{
+		i = 0;
+		philo = _philo(i);
+		while (i < data->nb_philo)
+		{
+			if (everyone_alive_and_hungry(philo) == FALSE)
+			{
+				print_death();
+				return ;
+			}
+			i++;
+		}
+		usleep(200);
+	}
+	
+}
 
 int	main(int ac, char **av)
 {
@@ -23,8 +48,8 @@ int	main(int ac, char **av)
 		free_all_data();
 		return (FAILURE);
 	}
+	watcher();
 	pthread_join_philo();
-	print_death();
 	free_all_data();
 	return (0);
 }
